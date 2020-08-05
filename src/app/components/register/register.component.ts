e9cbf3cdd9c9d95aa0ad9fbca6ac07800089c631
Claudio Giannetti <cagiannetti@gmail.com>
2020-08-05 12:52:45 -0300

    formulario de registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'register',
@@ -7,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
   public page_title: string;
+  public user: User;
 
   constructor() {
     this.page_title='Registrate';
+    this.user=new User(1, '', '', 'ROLE_USER', '', '', '',''); //creo un objeto user con propiedades por default que se van a modificar desde el form
+    /* propiedades de objeto user
+        public id: number,
+        public name: string,
+        public surname: string,
+        public role: string,
+        public email: string,
+        public password: string,
+        public description: string,
+        public image: string
+    */
   }
   
-    ngOnInit(): void {
+  ngOnInit(): void {
     console.log('componente de registro lanzado!');
+  }
+
+  onSubmit(form){ //cuando se envíe el formulario lo proceso acá
+    console.log(this.user);
+    form.reset();
   }
 
 }
